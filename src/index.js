17e46b3c44dd54c3366de0bf4f8b8ec44c88bd97
yuhsien <ex8600@gmail.com>
2021-07-10 18:20:59 +0800

    Add font-family and handle tab input

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import converter from './jsUglifier'
@@ -7,6 +7,24 @@ const Main = () => {
   const [value, setValue] = useState('')
   const [uglyValue, setUglyValue] = useState('')
   const [showCpExe, setShowCpExe] = useState(false)
+
+  useEffect(() => {
+    document.getElementById('ugly-input').addEventListener('keydown', function(e) {
+      if (e.key === 'Tab') {
+        e.preventDefault();
+        let start = this.selectionStart;
+        const end = this.selectionEnd;
+    
+        // set textarea value to: text before caret + tab + text after caret
+        this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
+    
+        // put caret at right position again
+        this.selectionStart = this.selectionEnd = start + 1;
+      }
+    });
+  }, [])
+
+
   const handleChange = (e) => {
     setValue(e.target.value)
   }
@@ -35,7 +53,7 @@ const Main = () => {
         <span className="uglifier">Uglifier</span>
       </h1>
       <div className="textarea-div">
-        <textarea value={value} onChange={handleChange} placeholder="Enter your JavaScript code here..." />
+        <textarea id="ugly-input" value={value} onChange={handleChange} placeholder="Enter your JavaScript code here..." />
       </div>
       <div className="uglifiy-btn-div">
         <button type="button" onClick={uglifiy}>Uglify</button>
