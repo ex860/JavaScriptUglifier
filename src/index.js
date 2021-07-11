9493137c857820e21e8ffd883a6b46bcc054c5d1
yuhsien <ex8600@gmail.com>
2021-07-11 16:45:59 +0800

    Add example codes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const Main = () => {
   const [value, setValue] = useState('')
   const [uglyValue, setUglyValue] = useState('')
   const [showCpExe, setShowCpExe] = useState(false)
+  const examples = [
+    `alert("Hello World!")`,
+
+    `const array = [1, 2, 3, 4, 5]\n` +
+    `const power = array.map(a => a * a)\n` +
+    `alert(power)`,
+
+    `const fib = (n) => {\n`+
+    `  if (n <= 0) return 0\n`+
+    `  else if (n === 1 || n === 2) return 1\n`+
+    `  else return fib(n - 1) + fib(n - 2)\n`+
+    `}\n`+
+    `alert([1,2,3,4,5,6,7,8,9,10].map(n => fib(n)))`,
+  ]
 
   useEffect(() => {
     document.getElementById('ugly-input').addEventListener('keydown', function (e) {
@@ -79,14 +93,23 @@ const Main = () => {
       (() => { eval(uglyValue) })()
     } catch (err) { console.error(err) }
   }
+
+  const renderExamples = () => {
+    return examples.map(example => {
+      return <pre onClick={() => setValue(example)}>{example}</pre>
+    })
+  }
   return (
     <>
-      <h1 className="title">
+      <div className="title">
         <span className="javascript">JavaScript</span>
         <span className="uglifier">Uglifier</span>
-      </h1>
+      </div>
+      <div className="example-div">
+        {renderExamples()}
+      </div>
       <div className="textarea-div">
-        <textarea id="ugly-input" value={value} onChange={handleChange} placeholder="Enter your JavaScript code here..." />
+        <textarea id="ugly-input" value={value} onChange={handleChange} placeholder="Enter your JavaScript code here... (Or click the examples above)" />
       </div>
       <div className="uglifiy-btn-div">
         <button type="button" onClick={uglifiy}>Uglify</button>
